d60bc3d7879e8c1303c5b952327e85200d8a547f
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-05-18 14:09:09 +0700

    modif: try create GetImageScreen

--- app/src/main/java/com/capstone/techwastemark01/ui/screen/GetImageScreen.kt
+++ app/src/main/java/com/capstone/techwastemark01/ui/screen/GetImageScreen.kt
@@ -1,2 +1,169 @@
 package com.capstone.techwastemark01.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.capstone.techwastemark01.R
+import com.capstone.techwastemark01.ui.component.AppTopBar
+import com.capstone.techwastemark01.ui.component.DefaultButton
+import com.capstone.techwastemark01.ui.component.DefaultOutlinedButton
+import com.capstone.techwastemark01.ui.theme.Sakura68
+import com.capstone.techwastemark01.ui.theme.TechWasteMark01Theme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GetImageScreen(
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        topBar = {
+            AppTopBar(
+                title = "Prediksi E-Waste",
+                onNavigationClick = { /* TODO */ },
+                onActionClick = { /*TODO*/ },
+                borderColor = MaterialTheme.colorScheme.primary,
+                circleColor = MaterialTheme.colorScheme.primary
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 40.dp, start = 16.dp, end = 16.dp),
+                contentAlignment = Alignment.TopCenter
+            ) {
+                Card(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.img_test),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(250.dp)
+                            .fillMaxWidth()
+                            .aspectRatio(1f)
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(92.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .background(Sakura68, CircleShape)
+                            .size(72.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = "Add",
+                            tint = Color.White
+                        )
+                    }
+                    IconButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .background(Sakura68, CircleShape)
+                            .size(72.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = "Delete",
+                            tint = Color.White
+                        )
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                DefaultButton(
+                    contentText = "Prediksi"
+                )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Default.Info,
+                        contentDescription = "Info",
+                        tint = Sakura68,
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "Sudah tau jenis e-waste anda?",
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+            ) {
+                DefaultOutlinedButton(
+                    contentText = "Pilih Jenis E-Waste"
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GetImageScreenPreview() {
+    TechWasteMark01Theme {
+        GetImageScreen()
+    }
+}
